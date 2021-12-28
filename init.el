d107b00ec183fa424f77dae7995060be5b12e964
Michael Lausch <mick.lausch@gmail.com>
2021-12-28 21:01:30 +0100

    Added webmode for react/typescript development.

--- init.el
+++ init.el
@@ -960,7 +960,9 @@
          (typescript-mode . lsp)
          (ruby-mode . lsp)
          (js-mode . lsp)
+         (yaml-mode . lsp)
          (typescript-mode . lsp)
+         (web-mode . lsp)
          (python-mode . (lambda ()
                           (require 'lsp-pylsp)
                           (lsp)))
@@ -1478,3 +1480,21 @@
 
 (use-package vterm
   :ensure t)
+
+(use-package web-mode
+  :ensure t
+  :mode (("\\.html?\\'" . web-mode)
+         ("\\.tsx\\'". web-mode)
+         ("\\.jsx\\'". web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-block-padding 2
+        web-mode-comment-style 2
+
+        web-mode-enable-css-colorization t
+        web-mode-enable-auto-pairing t
+        web-mode-enable-comment-keywords t
+        web-mode-enable-current-element-highlight t
+        ))
